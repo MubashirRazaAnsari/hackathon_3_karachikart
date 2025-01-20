0eb92ebba67bbd414735c108de873eab3c603dfb
MubashirRazaAnsari <ansarimubashir47@gmail.com>
2025-01-21 01:45:07 +0500

    TestReport Documentation and Components

---
+++ karachikart/jest.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from 'jest';
+import nextJest from 'next/jest';
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const config: Config = {
+  setupFilesAfterEnv: ['<rootDir>/__tests__/jest.setup.ts'],
+  testEnvironment: 'jest-environment-jsdom',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: [
+    '**/__tests__/**/*.test.[jt]s?(x)',
+  ],
+};
+
+export default createJestConfig(config); 
